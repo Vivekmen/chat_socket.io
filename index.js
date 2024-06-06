42d4fc6bc7c0ed8ace81090cd09a894a0532146f
Vivekmen <20ce052.vivek.menapara@vvpedulink.ac.in>
2024-06-06 14:43:03 +0530

    chat app

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const http = require("http");
+const app = express();
+const { Server } = require("socket.io");
+const path = require("path");
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "/public/index.html"));
+});
+
+const users = {};
+
+io.on("connection", (socket) => {
+  console.log("user connected");
+  socket.on("user-connect", (name) => {
+    users[socket.id] = name;
+
+    socket.broadcast.emit("user-joined", name);
+  });
+
+  socket.on("send", (message) => {
+
+    io.emit("reciever", { message: message, name: users[socket.id] });
+
+  });
+
+  socket.on('join room',(roomname)=>{
+     socket.join(roomname)
+  })
+
+  socket.on('room chat',(data,roomname)=>{
+    console.log(roomname);
+     io.to(roomname).emit('msg',data);
+  })
+});
+
+server.listen(6200, () => {
+  console.log("server is running at http://localhost:6200");
+});
